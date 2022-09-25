672a4e5adbc006f0de9fdc0652e1603b6a7a22a6
msi <msi@161.9.88.12>
2022-09-25 13:10:45 +0300

    Variables

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,25 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String message = "Öğrenci sayısı : ";
+		int ogrenciSayisi = 10;
+		
+		//System.out.println("Öğrenci sayısı : "+ ogrenciSayisi);
+		//System.out.println("Öğrenci sayısı : "+ ogrenciSayisi);
+		//System.out.println("Öğrenci sayısı : "+ ogrenciSayisi);
+		//System.out.println("Öğrenci sayısı : "+ ogrenciSayisi);
+		//System.out.println("Öğrenci sayısı : "+ ogrenciSayisi);
+		
+		System.out.println(message + ogrenciSayisi);
+		
+		
+		
+		
+		
+
+	}
+
+}
