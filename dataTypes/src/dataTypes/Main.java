08f242324c83d9ebcdb41baa65f978e7f2f1e8aa
msi <msi@160.75.201.226>
2022-09-26 23:21:24 +0300

    data types

---
+++ dataTypes/src/dataTypes/Main.java
@@ -0,0 +1,14 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+	double sayi = 12.5;
+	       sayi = -128;
+	       
+	char karakter = 'A';
+	
+	boolean dogruMu = false;
+	
+	}
+}
