aac1b959f27017d748c8e0c64049f089dcbff628
msi <msi@160.75.201.226>
2022-09-27 23:35:41 +0300

    loop demo

---
+++ loopDemo/src/loopDemo/Main.java
@@ -0,0 +1,12 @@
+package loopDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//For döngüsüyle 10'dan küçük ve 3'ün katı olan sayıları yazdırıyoruz
+		for (int i = 3;i < 10;i+=3) {
+			System.out.println(i);
+		}
+		System.out.println("Döngü sona erdi");
+	}
+}
