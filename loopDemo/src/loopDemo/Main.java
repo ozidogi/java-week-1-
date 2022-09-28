60370d77b06658c045443e37f51fabbb13437e86
msi <msi@160.75.201.226>
2022-09-28 22:54:35 +0300

    loop demo

--- loopDemo/src/loopDemo/Main.java
+++ loopDemo/src/loopDemo/Main.java
@@ -7,6 +7,23 @@ public class Main {
 		for (int i = 3;i < 10;i+=3) {
 			System.out.println(i);
 		}
-		System.out.println("Döngü sona erdi");
+		System.out.println("For döngüsü sona erdi");
+		
+		//While döngüsü
+		int i = 3;
+		while (i < 10) {
+			System.out.println(i);
+			i = i+3;
+		}
+		System.out.println("While döngüsü sona erdi");	
+			
+		//Do-while döngüsü
+		int j = 3;
+		do {System.out.println(j);
+			j = j+3;
+			}
+		while (j < 10);
+		System.out.println("Do-while döngüsü sona erdi");
+				
 	}
 }
